af85a2e9d0464e0b645545f5124f6a1bfb8e869b
Casinhandrade <casinhandrade@hotmail.com>
2018-07-31 17:27:04 +0100

    happy

--- src/tryingGit/Main.java
+++ src/tryingGit/Main.java
@@ -11,6 +11,15 @@ public class Main {
 		System.out.println("hello from master branch");
 		
 		System.out.println("I love amilcar in the new branck");
+	
+System.out.println("Hello world");
+		
+		System.out.println("I love amilcar");
+		
+		System.out.println("hello from master branch");
+		
+		System.out.println("I love amilcar in the new branck");
+	
 	}
 
 }
