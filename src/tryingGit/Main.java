43bbae371dc6539eecf9e8e029deea907745ed5f
Casinhandrade <casinhandrade@hotmail.com>
2018-07-31 17:11:14 +0100

    I added two lines of very good code

--- src/tryingGit/Main.java
+++ src/tryingGit/Main.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		
+		System.out.println("I hate amilcar");
+		
+		System.out.println("I hate amilcar");
+		
 	}
 
 }
